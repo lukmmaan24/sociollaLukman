97f2c151d36907b8a18b5a4df3e341c7cd36a2f8
mac m1 max <kursimejakursi24@gmail.com>
2024-08-08 21:17:39 +0700

    loading spinner

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity, ActivityIndicator, ScrollView } from 'react-native';
 import axios from 'axios';
 import FeaturedBrands from './src/Components/FeaturedBrands';
 import AlphabetList from './src/Components/AlphabetList';
@@ -15,7 +15,7 @@ const App = () => {
   const [selectedTab, setSelectedTab] = useState('BRAND NAME');
   const [selectedLetter, setSelectedLetter] = useState('A');
   const [selectedCountry, setSelectedCountry] = useState('');
-  const [loading, setLoading] = useState(false); // Loading state
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchFeaturedBrands();
@@ -31,19 +31,19 @@ const App = () => {
   }, [selectedLetter, searchText, selectedTab, selectedCountry]);
 
   const fetchFeaturedBrands = async () => {
-    setLoading(true); // Start loading
+    setLoading(true);
     try {
       const response = await axios.get('https://catalog-api3.sociolla.com/v3/brands?filter={"is_featured":true}&fields=_id+name+slug+logo+featured_text&skip=0&limit=15&sort=-featured_created_at');
       setFeaturedBrands(response.data.data);
     } catch (error) {
       console.error('Error fetching featured brands:', error);
     } finally {
-      setLoading(false); // End loading
+      setLoading(false);
     }
   };
 
   const fetchBrands = async () => {
-    setLoading(true); // Start loading
+    setLoading(true);
     try {
       let url = 'https://catalog-api3.sociolla.com/v3/brands?fields=_id+name+slug+country+logo+featured_text&skip=0&limit=100&sort=name&';
 
@@ -57,14 +57,12 @@ const App = () => {
       } else if (selectedTab === 'BRAND ORIGINS') {
         url += `filter={"country":"${selectedCountry.toLowerCase()}"}`;
       }
-      console.log(url);
-
       const response = await axios.get(url);
       setBrands(response.data.data || []);
     } catch (error) {
       console.error('Error fetching brands:', error);
     } finally {
-      setLoading(false); // End loading
+      setLoading(false);
     }
   };
 
@@ -77,30 +75,32 @@ const App = () => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <FeaturedBrands brands={featuredBrands} />
-        {loading ? (
-          <ActivityIndicator size="large" color="#0000ff" />
-        ) : (
-          <>
-            <View style={styles.tabContainer}>
-              <TouchableOpacity onPress={() => handleTabChange('BRAND NAME')}>
-                <Text style={[styles.tabText, selectedTab === 'BRAND NAME' && styles.activeTabText]}>BRAND NAME</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => handleTabChange('BRAND ORIGINS')}>
-                <Text style={[styles.tabText, selectedTab === 'BRAND ORIGINS' && styles.activeTabText]}>BRAND ORIGINS</Text>
-              </TouchableOpacity>
-            </View>
-            {selectedTab === 'BRAND NAME' ? (
-              <SearchBar searchText={searchText} setSearchText={setSearchText} />
-            ) : (
-              <CountryPicker setSelectedCountry={setSelectedCountry} />
-            )}
-            {selectedTab === 'BRAND NAME' && !searchText && <AlphabetList setSelectedLetter={setSelectedLetter} />}
-            <BrandList brands={brands} />
-          </>
-        )}
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <FeaturedBrands brands={featuredBrands} />
+          {loading ? (
+            <ActivityIndicator size="large" color="#0000ff" />
+          ) : (
+            <>
+              <View style={styles.tabContainer}>
+                <TouchableOpacity onPress={() => handleTabChange('BRAND NAME')}>
+                  <Text style={[styles.tabText, selectedTab === 'BRAND NAME' && styles.activeTabText]}>BRAND NAME</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleTabChange('BRAND ORIGINS')}>
+                  <Text style={[styles.tabText, selectedTab === 'BRAND ORIGINS' && styles.activeTabText]}>BRAND ORIGINS</Text>
+                </TouchableOpacity>
+              </View>
+              {selectedTab === 'BRAND NAME' ? (
+                <SearchBar searchText={searchText} setSearchText={setSearchText} />
+              ) : (
+                <CountryPicker setSelectedCountry={setSelectedCountry} />
+              )}
+              {selectedTab === 'BRAND NAME' && !searchText && <AlphabetList setSelectedLetter={setSelectedLetter} />}
+              <BrandList brands={brands} />
+            </>
+          )}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
